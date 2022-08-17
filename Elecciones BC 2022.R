09fda1a54bcc44884b9405bbe5d2d6541f3089d0
marloz24 <62977015+marloz24@users.noreply.github.com>
2022-08-17 01:16:18 -0500

    Add files via upload

--- Elecciones BC 2022.R
+++ Elecciones BC 2022.R
@@ -21,6 +21,10 @@ BC_Gubernatura_2019 <- fread("https://raw.githubusercontent.com/marloz24/Eleccio
 BC_Gubernatura_2021 <- fread("https://raw.githubusercontent.com/marloz24/Elecciones-BC-2022/main/Resultados%20Campanas/BC_Gobernatura_2021.csv")
 
 
+# ====================================================================================================
+# ====================================================================================================
+
+
 #We notice abstention and participation are char variable because of % replace and convert to numeric
 BC_Ayuntamiento_2010 <- data.frame(lapply(BC_Ayuntamiento_2010, function(x) gsub("%", "", x)))
 BC_Ayuntamiento_2013 <- data.frame(lapply(BC_Ayuntamiento_2013, function(x) gsub("%", "", x)))
@@ -48,23 +52,69 @@ BC_Diputados_2010[,5:16] <- sapply(BC_Diputados_2010[,5:16], as.numeric)
 BC_Diputados_2013[,5:13] <- sapply(BC_Diputados_2013[,5:13], as.numeric)
 BC_Diputados_2016[,5:41] <- sapply(BC_Diputados_2016[,5:41], as.numeric)
 BC_Diputados_2019[,5:32] <- sapply(BC_Diputados_2019[,5:32], as.numeric)
-BC_Diputados_2019[,5:31] <- sapply(BC_Diputados_2019[,5:31], as.numeric)
+BC_Diputados_2021[,5:31] <- sapply(BC_Diputados_2021[,5:31], as.numeric)
 
 BC_Gubernatura_2013[,5:13] <- sapply(BC_Gubernatura_2013[,5:13], as.numeric)
 BC_Gubernatura_2019[,5:30] <- sapply(BC_Gubernatura_2019[,5:30], as.numeric)
 BC_Gubernatura_2021[,5:29] <- sapply(BC_Gubernatura_2021[,5:29], as.numeric)
 
-# A few NAs have arised, we will convert them to zeros
-BC_Ayuntamiento_2013[is.na(BC_Ayuntamiento_2013)] <- 0
-BC_Ayuntamiento_2016[is.na(BC_Ayuntamiento_2013)] <- 0
-BC_Diputados_2013[is.na(BC_Diputados_2016)] <- 0
 
-#
+# ====================================================================================================
+# ====================================================================================================
+
+
+# We convert all NAs in all tables to 0
+BC_Ayuntamiento_2010[is.na(BC_Ayuntamiento_2010)] <- 0
+BC_Ayuntamiento_2013[is.na(BC_Ayuntamiento_2013)] <- 0
+BC_Ayuntamiento_2016[is.na(BC_Ayuntamiento_2016)] <- 0
+BC_Ayuntamiento_2019[is.na(BC_Ayuntamiento_2019)] <- 0
+BC_Ayuntamiento_2021[is.na(BC_Ayuntamiento_2021)] <- 0
+
+BC_Diputados_2010[is.na(BC_Diputados_2010)] <- 0
+BC_Diputados_2013[is.na(BC_Diputados_2013)] <- 0
+BC_Diputados_2016[is.na(BC_Diputados_2016)] <- 0
+BC_Diputados_2019[is.na(BC_Diputados_2019)] <- 0
+BC_Diputados_2021[is.na(BC_Diputados_2021)] <- 0
+
+BC_Gubernatura_2013[is.na(BC_Gubernatura_2013)] <- 0
+BC_Gubernatura_2019[is.na(BC_Gubernatura_2019)] <- 0
+BC_Gubernatura_2021[is.na(BC_Gubernatura_2021)] <- 0
+
+
+
+# ====================================================================================================
+# ====================================================================================================
+
+
+#Before formatting data we check that results match to those published by IEEBC
+aggregate(BC_Ayuntamiento_2010$CABC, by = list(Category = BC_Ayuntamiento_2010$MUNICIPIO), FUN=sum)
+aggregate(BC_Diputados_2010$CABC, by = list(Category = BC_Diputados_2010$MUNICIPIO), FUN=sum)
+
+aggregate(BC_Ayuntamiento_2013$UNIDOS.POR.BAJA.CALIFORNIA, by = list(Category = BC_Ayuntamiento_2013$MUNICIPIO), FUN=sum)
+aggregate(BC_Diputados_2013$UNIDOS.POR.BAJA.CALIFORNIA, by = list(Category = BC_Diputados_2013$MUNICIPIO), FUN=sum)
+aggregate(BC_Gubernatura_2013$UNIDOS.POR.BAJA.CALIFORNIA, by = list(Category = BC_Gubernatura_2013$MUNICIPIO), FUN=sum)
+
+aggregate(BC_Ayuntamiento_2016$PAN, by = list(Category = BC_Ayuntamiento_2016$MUNICIPIO), FUN=sum)
+aggregate(BC_Diputados_2016$PAN, by = list(Category = BC_Diputados_2016$MUNICIPIO), FUN=sum)
+
+aggregate(BC_Ayuntamiento_2019$PAN, by = list(Category = BC_Ayuntamiento_2019$MUNICIPIO), FUN=sum)
+aggregate(BC_Diputados_2019$PAN, by = list(Category = BC_Diputados_2019$MUNICIPIO), FUN=sum)
+aggregate(BC_Gubernatura_2019$PAN, by = list(Category = BC_Gubernatura_2019$MUNICIPIO), FUN=sum)
+
+aggregate(BC_Ayuntamiento_2021$PAN, by = list(Category = BC_Ayuntamiento_2021$MUNICIPIO), FUN=sum)
+aggregate(BC_Diputados_2021$PAN, by = list(Category = BC_Diputados_2021$MUNICIPIO), FUN=sum)
+aggregate(BC_Gubernatura_2021$PAN, by = list(Category = BC_Gubernatura_2021$MUNICIPIO), FUN=sum)
+
+
+# ====================================================================================================
+# ====================================================================================================
+
+# After validating csv match final published results, we format and standardized  our tables
 colnames(BC_Ayuntamiento_2010)
 colnames(BC_Diputados_2010)
-BC_Diputados_2010 <- subset(BC_Diputados_2010, select=-c(V15, V16))
-setnames(BC_Ayuntamiento_2010, old = c("CABC", "CGR", "CRBC"), new = c("PAN", "PRI", "PT"))
-setnames(BC_Diputados_2010, old = c("CABC", "CGR", "CRBC"), new = c("PAN", "PRI", "PT"))
+#BC_Diputados_2010 <- subset(BC_Diputados_2010, select=-c(V16))
+setnames(BC_Ayuntamiento_2010, old = c("CABC", "CGR", "CRBC"), new = c("Pre - ", "Pre - PRI", "PT"))
+setnames(BC_Diputados_2010, old = c("CABC", "CGR", "CRBC"), new = c("Pre - ", "Pre - PRI", "PT"))
 
 colnames(BC_Ayuntamiento_2013)
 colnames(BC_Diputados_2013)
@@ -78,9 +128,9 @@ setnames(BC_Gubernatura_2013, old = c("UNIDOS.POR.BAJA.CALIFORNIA", "COMPROMISO.
 
 colnames(BC_Ayuntamiento_2016)
 colnames(BC_Diputados_2016)
-setnames(BC_Ayuntamiento_2016, old = c("PAN", "PRI", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8","C9", "C10", "C11"), 
+setnames(BC_Ayuntamiento_2016, old = c("", "PRI", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8","C9", "C10", "C11"), 
          new = c("Pre - PAN", "Pre - PRI", rep(c("Pre - PRI"),each=11)))
-setnames(BC_Diputados_2016, old = c("C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8","C9", "C10", "C11"), 
+setnames(BC_Diputados_2016, old = c("", "PRI", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8","C9", "C10", "C11"), 
          new = c("Pre - PAN", "Pre - PRI", rep(c("Pre - PRI"),each=11)))
 
 
@@ -104,19 +154,14 @@ setnames(BC_Ayuntamiento_2021, old = c("PAN", "PRI", "PRD", "PT", "PVEM", "PBC",
                  "Pre - PBC", "Pre - MC", "Pre - Morena", "Pre - PES", "Pre - RSP", "Pre - FXM",
                  rep("Pre - Coalicion", each = 8)))
 setnames(BC_Diputados_2021, old = c("PAN", "PRI", "PRD", "PT", "PVEM", "PBC", "MC", "MORENA", "PES", "RSP", 
-                                       "FXM", "PAN...PRI...PRD", "PAN...PRI", "PAN...PRD", "PRI.PRD", 
-                                       "PT.PVEM.MORENA", "PT.PVEM", "PT...MORENA", "PVEM...MORENA"), 
+                                    "FXM", "PAN...PRI...PRD", "PAN...PRI", "PAN...PRD", "PRI.PRD", 
+                                    "PT.PVEM.MORENA", "PT.PVEM", "PT...MORENA", "PVEM...MORENA"), 
          new = c("Pre - PAN", "Pre - PRI", "Pre - PRD", "Pre - PT", "Pre - PVEM", 
                  "Pre - PBC", "Pre - MC", "Pre - Morena", "Pre - PES", "Pre - RSP", "Pre - FXM",
                  rep("Pre - Coalicion", each = 8)))
 setnames(BC_Gubernatura_2021, old = c("PAN", "PRI", "PRD", "PT", "PVEM", "PBC", "MC", "MORENA", "PES", "RSP", 
-                                       "FXM", "PAN...PRI...PRD", "PAN...PRI", "PAN...PRD", "PRI.PRD", 
-                                       "PT.PVEM.MORENA", "PT.PVEM", "PT...MORENA", "PVEM...MORENA"), 
+                                      "FXM", "PAN...PRI...PRD", "PAN...PRI", "PAN...PRD", "PRI.PRD", 
+                                      "PT.PVEM.MORENA", "PT.PVEM", "PT...MORENA", "PVEM...MORENA"), 
          new = c("Pre - PAN", "Pre - PRI", "Pre - PRD", "Pre - PT", "Pre - PVEM", 
                  "Pre - PBC", "Pre - MC", "Pre - Morena", "Pre - PES", "Pre - RSP", "Pre - FXM",
                  rep("Pre - Coalicion", each = 8)))
-
-
-#Before formatting data we check that results match to those published by IEEBC 
-aggregate(BC_Ayuntamiento_2010$CABC, by=list(Category = BC_Ayuntamiento_2010$MUNICIPIO), FUN=sum)
-aggregate(BC_Diputados_2010$CABC, by=list(Category=BC_Diputados_2010$MUNICIPIO), FUN=sum)
